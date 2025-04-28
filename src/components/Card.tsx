9d4063dedb6889ef04d5339f99433c2dbc1c3442
Your NameCristiana <papercri@gmail.com>
2025-04-28 10:47:01 +0200

    cortrections

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -19,40 +19,43 @@ function Card({ title, description, bgColor, image, clickNext, isLast, clickPrev
   return (
     <>
       <div className="grid place-items-center min-h-screen">
-        <div className="card grid grid-rows-[2fr_1fr] gap-4 shadow-2xl bg-white">
-          <div
-          key={currentStep}
-          className={`img flex justify-center items-center transition-all duration-500 ease-in-out opacity-0 animate-slide-fade ${
+      <div
+        key={currentStep}
+        className={`card grid grid-rows-[2fr_1fr] gap-4 shadow-2xl bg-white transition-all duration-500 ease-in-out animate-slide-fade ${
           direction === "right" ? "slide-from-right" : "slide-from-left"
-          }`}
+        }`}
+      >
+        <div
+          className="img flex justify-center items-center"
           style={{ backgroundColor: bgColor }}
-          >
-              <img src={image} alt={title} />
+        >
+          <img src={image} alt={title} />
+        </div>
+
+        <div className="desc flex flex-col justify-between">
+          <div className="text-left opacity-0 animate-fade-in-up">
+            <h2>{title}</h2>
+            <p>{description}</p>
           </div>
-          <div className="desc flex flex-col justify-between">
-          <div key={currentStep} className="text-left opacity-0 animate-fade-in-up">
-              <h2>{title}</h2>
-              <p>{description}</p>
+          <div className="flex justify-between gap-2.5 mt-5">
+            <div className="bullets flex justify-start gap-2.5 ">
+              <Indicator 
+                currentStep={currentStep} 
+                toStep={toStep} 
+              />
             </div>
-            <div className="flex justify-between gap-2.5 mt-5">
-              <div className="bullets flex justify-start gap-2.5 ">
-                <Indicator 
-                  currentStep={currentStep} 
-                  toStep={toStep} 
-                />
-              </div>
-              <div className="buttons flex justify-end gap-2.5 ">
-                <Buttons 
-                  clickPrev={handlePrev} 
-                  clickNext={handleNext} 
-                  isFirst={isFirst} 
-                  isLast={isLast} 
-                />
-              </div>
+            <div className="buttons flex justify-end gap-2.5 ">
+              <Buttons 
+                clickPrev={handlePrev} 
+                clickNext={handleNext} 
+                isFirst={isFirst} 
+                isLast={isLast} 
+              />
             </div>
           </div>
         </div>
       </div>
+    </div>
 
     </>
   );
